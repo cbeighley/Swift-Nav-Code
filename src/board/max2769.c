f469d10b63119bcaedf8a3c97dcac734cb8d825d
Colin Beighley <colinbeighley@gmail.com>
2015-02-02 14:06:36 -0800

    Change to correct PLL setting for fractional for L5 LO of 1556.45MHz

--- src/board/max2769.c
+++ src/board/max2769.c
@@ -139,7 +139,7 @@ void max2769_configure(void)
 
   max2769_fdiv = MAX2769_FDIV_RESERVED |
 //                 MAX2769_FDIV_FDIV(0);
-                 MAX2769_FDIV_FDIV(471859);
+                 MAX2769_FDIV_FDIV(583270);
   max2769_write(MAX2769_FDIV, max2769_fdiv);
 
   max2769_clk = MAX2769_CLK_L_CNT(1) |
